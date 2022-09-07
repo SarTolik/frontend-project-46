b9d2bbbd45d941a7b2a633bca9e66f8c29451486
SarTolik <sartolik@gmail.com>
2022-09-07 20:12:40 +0300

    add lint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,10 +4,10 @@ import { program } from 'commander';
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.');
 
 program
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format');
 
-  program.parse();
+program.parse();
