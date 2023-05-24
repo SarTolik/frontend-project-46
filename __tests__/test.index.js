ac5eaf067ca3a49f01fb28963be32e759808cda2
SarTolik <sartolik@gmail.com>
2023-05-24 19:37:48 +0300

    diff yaml

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -7,11 +7,22 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
-
-const filepath1 = getFixturePath('file1.json');
-const filepath2 = getFixturePath('file2.json');
 const expected = readFile('true.txt');
 
-test('genDiff', () => {
+test('genDiff JSON', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  expect(genDiff(filepath1, filepath2)).toEqual(expected);
+});
+
+test('genDiff YAML', () => {
+  const filepath1 = getFixturePath('file1.yaml');
+  const filepath2 = getFixturePath('file2.yaml');
+  expect(genDiff(filepath1, filepath2)).toEqual(expected);
+});
+
+test('genDiff YML', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.yml');
   expect(genDiff(filepath1, filepath2)).toEqual(expected);
 });
--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import findDiff from './findDiff.js';
+import parse from './parsers.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const getFormat = (file) => path.extname(file).slice(1);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
 const genDiff = (file1, file2) => {
   const data1 = readFile(file1);
   const data2 = readFile(file2);
+  const fileFormat1 = getFormat(file1);
+  const fileFormat2 = getFormat(file2);
 
-  const parseData1 = JSON.parse(data1);
-  const parseData2 = JSON.parse(data2);
+  const parseData1 = parse(data1, fileFormat1);
+  const parseData2 = parse(data2, fileFormat2);
   return findDiff(parseData1, parseData2);
 };
 
